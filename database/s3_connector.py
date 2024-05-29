c054417d77e4a3905b1f746bc1f9543500be2925
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2024-05-29 16:53:20 +0700

    change the tests

--- database/s3_connector.py
+++ database/s3_connector.py
@@ -5,10 +5,10 @@ class S3Connector:
     def __init__(self):
         self.s3 = boto3.client(
             "s3",
-            aws_access_key_id=S3Config.S3_ACCESS_KEY,
-            aws_secret_access_key=S3Config.S3_SECRET_KEY,
-            endpoint_url="http://localhost:9000",
-            use_ssl=False,
+            # aws_access_key_id=S3Config.S3_ACCESS_KEY,
+            # aws_secret_access_key=S3Config.S3_SECRET_KEY,
+            # endpoint_url="http://localhost:9000",
+            # use_ssl=False,
         )
         
     def get_object(self, bucket: str, key: str) -> None:
--- tests/s3_connector_test.py
+++ tests/s3_connector_test.py
@@ -1,25 +1,40 @@
-import pytest
-from unittest.mock import MagicMock
+from moto import mock_aws
 from database.s3_connector import S3Connector
+import pytest
+
 
 @pytest.fixture
 def s3_connector():
-    return S3Connector()
+    with mock_aws():
+        yield S3Connector()
 
-def test_get_object(s3_connector):
-    # Mock the boto3 client's get_object method
-    s3_connector.s3.get_object = MagicMock(return_value={"Body": "test body"})
-    
-    # Call the get_object method and assert the result
-    result = s3_connector.get_object("linkedin-data", "test-key")
-    assert result == {"Body": "test body"}
-    s3_connector.s3.get_object.assert_called_once_with(Bucket="linkedin-data", Key="test-key")
 
+@mock_aws
 def test_put_object(s3_connector):
-    # Mock the boto3 client's put_object method
-    s3_connector.s3.put_object = MagicMock()
-    
-    # Call the put_object method and assert the result
+    s3_connector.s3.create_bucket(Bucket="linkedin-data")
+
     result = s3_connector.put_object("linkedin-data", "test-key", "test body")
-    assert result is None
-    s3_connector.s3.put_object.assert_called_once_with(Bucket="linkedin-data", Key="test-key", Body="test body")
+
+    # Assert the success of the upload by checking if a specific response key exists
+    assert "ETag" in result
+
+    # Retrieve the uploaded object to verify its contents
+    obj = s3_connector.s3.get_object(Bucket="linkedin-data", Key="test-key")
+    assert obj["Body"].read().decode("utf-8") == "test body"
+
+
+@mock_aws
+def test_get_object(s3_connector):
+    s3_connector.s3.create_bucket(Bucket="linkedin-data")
+
+    s3_connector.put_object("linkedin-data", "test-key", "test body")
+
+    # Test the get_object method
+    obj = s3_connector.get_object("linkedin-data", "test-key")
+
+    # Check if obj is not none and is a dictionary
+    assert obj and isinstance(obj, dict)
+
+    # Assert that the body of the object matches the expected content
+    assert obj.get("Body") is not None
+    assert obj["Body"].read().decode("utf-8") == "test body"
